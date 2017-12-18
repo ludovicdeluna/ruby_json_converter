ac5c918237c3e1ed7bce7dee2312e5fe0f31a005
Ludovic de Luna <ludovic@deluna.fr>
2017-12-18 21:27:32 +0100

    Switch from exploration to ruby lib
    (Forget json_converter.gemspec from first comit)

---
+++ json_converter.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = "json_converter"
+  s.version     = "1.0.0"
+  s.summary     = "JSON Converter"
+  s.licenses    = ["MIT"]
+  s.description = "Convert JSON to CSV"
+  s.authors     = ["Ludovic"]
+  s.email       = "ludovic.deluna@gmail.com"
+  s.homepage    = "http://github.com/ludovicdeluna/json_converter"
+  s.files       = ["lib/json_converter.rb"]
+  s.required_ruby_version = '~> 2.0', '>= 2.3'
+end
---
+++ test/test_json_converter.rb
@@ -0,0 +1,34 @@
+require 'minitest/autorun'
+require 'json_converter'
+
+class TestJsonConverter < Minitest::Test
+  attr_reader :json_converter
+
+  def setup
+    @json_converter = JsonConverter.new('test/fixtures/users.json')
+  end
+
+  def test_headers
+    expected = [
+      "id", "email", "tags", "profiles.facebook.id",
+      "profiles.facebook.picture", "profiles.twitter.id",
+      "profiles.twitter.picture"
+    ]
+    assert_equal expected, json_converter.headers
+  end
+
+  def test_diggers
+    expected = [
+      ["id"], ["email"], ["tags"], ["profiles", "facebook", "id"],
+      ["profiles", "facebook", "picture"], ["profiles", "twitter", "id"],
+      ["profiles", "twitter", "picture"]
+    ]
+    assert_equal expected, json_converter.diggers
+  end
+
+  def test_to_csv
+    skip
+    expected = File.read("test/fixtures/users.csv")
+    assert_equal expected, json_converter.to_csv
+  end
+end
