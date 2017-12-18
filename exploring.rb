61953c782a491849276f42f6defe076cf647b262
Ludovic de Luna <ludovic@deluna.fr>
2017-12-18 21:19:15 +0100

    With dot separator
    Also get an array to easy use with dig method (for further works)

--- exploring.rb
+++ exploring.rb
@@ -4,12 +4,17 @@ require 'pp'
 json = JSON.load(File.read('users.json'))
 
 
-def get_deep_keys(obj)
-  obj.map do |k, v|
-    other_keys = get_deep_keys(obj[k]) if v.respond_to?(:keys)
-    other_keys ? {k => other_keys} : k
+# TODO :
+# Limitation : field can't contain dot in it's name. Throw error in this case.
+def get_deep_keys(hsh)
+  hsh.each_with_object([]) do |(field, value), acc|
+    other_keys = get_deep_keys(hsh[field]) if value.respond_to?(:keys)
+    other_keys ? other_keys.each { |k| acc << "#{field}.#{k}" } : acc << field
   end
 end
 
-pp get_deep_keys(json.first)
+headers = get_deep_keys(json.first)
+diggers = headers.map { |v| v.split('.') }
 
+pp headers
+pp diggers # TODO : Limitation : Structure dismatch, on empty dig result, throw error.
